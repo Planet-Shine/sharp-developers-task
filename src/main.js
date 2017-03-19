4daf1a3ffb85a662b233d976e4f37be741f271eb
PavelOfficial <pavelofficial@ya.ru>
2017-03-19 21:46:27 +0300

    ie9 fix

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
+import from from 'array.from';
+from.shim();
 
 import React from 'react';
 import ReactDOM from 'react-dom';
